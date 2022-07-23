30b895ec350bb60c9472286cee1e3cf3a15c5b58
choirul afiq <191240000930@unisnu.ac.id>
2022-07-23 22:59:40 +0700

    koneksi

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
 const express = require ('express')
+const colors = require ('colors')
 const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
+const { errorHandler } = require( './middleware/errorMiddleware' )
 const port = process.env.PORT || 5000
 
-
-const app = express()
-app.get('/api/goals',(req,res)=>{
-    res.json({message: 'Get Goals'})
-
-})
-
+connectDB()
+const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
 
-app.use('/api/goals',require('./routes/goalRoutes.js'))
+app.use( '/api/goals' ,require( './routes/goalRoutes.js' ))
+
+app.use(errorHandler)
 
 app.listen(port,() => console.log('Server started on port ${port}'))
