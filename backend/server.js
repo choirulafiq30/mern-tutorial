514d7e470d60ac4822bf6f19f1b1b26ce088c85f
choirul afiq <191240000930@unisnu.ac.id>
2022-07-24 10:32:52 +0700

    asyn

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const colors = require('colors')
+const express = require ('express')
+const colors = require ('colors')
 const dotenv = require('dotenv').config()
 const connectDB = require('./config/db')
-const { errorHandler } = require('./middleware/errorMiddleware')
+const { errorHandler } = require( './middleware/errorMiddleware' )
 const port = process.env.PORT || 5000
 
 connectDB()
@@ -10,9 +10,9 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
 
-app.use('/api/goals',require('./routes/goalRoutes.js'))
-app.use('/api/users',require('./routes/userRoutes.js'))
+app.use( '/api/goals' ,require( './routes/goalRoutes.js' ))
+app.use( '/api/users' ,require( './routes/goalRoutes.js' ))
 
 app.use(errorHandler)
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(port,() => console.log('Server started on port ${port}'))
